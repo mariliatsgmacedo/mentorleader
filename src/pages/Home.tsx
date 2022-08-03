fd5d2cd952c95999192712e9eb8fbd70ff58f66c
Jonatas Oliveira Da Silva <vntjoos@JonatasOliveira.local>
2022-08-03 09:08:19 -0300

    Fixed import issue on Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,37 +1,38 @@
-import { Banner } from '../components/banner-default/Banner';
-import { CardBanner } from '../components/card-banner/CardBanner';
-import { Footer } from '../components/footer/Footer';
-import { Header } from '../components/header-menu/Header';
-import { MentorListCard } from '../components/mentor-card/MentorListCard';
-import { ModalMentor } from '../components/modal-schema/Modal';
-import { BannerFixed } from '../components/articles/BannerFixed';
-import { Outlet } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import { getUserAuth, getUserInfo } from '../services/firebase/firebase';
-import { Typography } from '@mui/material';
-
+import { Typography } from "@mui/material";
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
+import { BannerFixed } from "../components/articles/BannerFixed";
+import { Banner } from "../components/banner-default/Banner";
+import { CardBanner } from "../components/card-banner/CardBanner";
+import { Footer } from "../components/Footer/Footer";
+import { Header } from "../components/header-menu/Header";
+import { MentorListCard } from "../components/mentor-card/MentorListCard";
 
 export const Home = () => {
-    const [open, setOpen] = useState(false);
-    const describeBanner = "Já pensou em ajudar alguém que está começando a aprender a programar? Ou em ajudar outra pessoa a desenvolver melhor skills que você já desenvolveu? Então, essa é uma daquelas oportunidades. Estamos aqui para desenvolver a cultura de compartilhamento de conhecimento. Se você procura desenvolver alguma skill basta procurar um dos nossos mentores. Lembrando que quem faz a mentoria são vocês, os mentores são guias, ok?"
-   
+  const [open, setOpen] = useState(false);
+  const describeBanner =
+    "Já pensou em ajudar alguém que está começando a aprender a programar? Ou em ajudar outra pessoa a desenvolver melhor skills que você já desenvolveu? Então, essa é uma daquelas oportunidades. Estamos aqui para desenvolver a cultura de compartilhamento de conhecimento. Se você procura desenvolver alguma skill basta procurar um dos nossos mentores. Lembrando que quem faz a mentoria são vocês, os mentores são guias, ok?";
 
-    function handleModalOpen() {
-        setOpen(true);
-    }
-    function handleModalClose() {
-        setOpen(false);
-    }
+  function handleModalOpen() {
+    setOpen(true);
+  }
+  function handleModalClose() {
+    setOpen(false);
+  }
 
-    return <>
-        <Header />
-        <Banner describe={describeBanner}/>
-        <CardBanner />
-        <Typography align='center' variant='h5'>Mentores Disponíveis</Typography>
-        <MentorListCard onMentorModalIsOpen={handleModalOpen}  />
-        <BannerFixed />
-     
-        <Footer />
-        <Outlet />
+  return (
+    <>
+      <Header />
+      <Banner describe={describeBanner} />
+      <CardBanner />
+      <Typography align="center" variant="h5">
+        Mentores Disponíveis
+      </Typography>
+      <MentorListCard onMentorModalIsOpen={handleModalOpen} />
+      <BannerFixed />
+
+      <Footer />
+      <Outlet />
     </>
-}
+  );
+};
